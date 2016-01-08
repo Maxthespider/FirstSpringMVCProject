2d0d4b54b521211e1167a63262459499a97f3201
Hiero <kazsik.adam@gmail.com>
2016-01-08 16:12:59 +0100

    add @PathVariable annotation

--- src/com/hiero/hellocontroller/HelloController.java
+++ src/com/hiero/hellocontroller/HelloController.java
@@ -1,28 +1,26 @@
 package com.hiero.hellocontroller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/greet")
 public class HelloController {
 	
-	@RequestMapping("/welcome")
-	public ModelAndView helloWorld(){
+	@RequestMapping("/welcome/{countryName}/{userName}")
+	//public ModelAndView helloWorld(@PathVariable("userName") String name,@PathVariable("countryName") String country){
+	public ModelAndView helloWorld(@PathVariable Map<String, String> pathVars){
+		
+		String name = pathVars.get("userName");
+		String country = pathVars.get("countryName");
 		
 		ModelAndView model = new ModelAndView("HelloPage");
-		model.addObject("msg", "Hello World!");
+		model.addObject("msg", "Hello " + name + "!" + " You are from " + country);
 		
 		return model;	
 	}
 	
-	@RequestMapping("/hi")
-	public ModelAndView hiWorld(){
-		
-		ModelAndView model = new ModelAndView("HelloPage");
-		model.addObject("msg", "Hi World!");
-		
-		return model;
-	}
 }
