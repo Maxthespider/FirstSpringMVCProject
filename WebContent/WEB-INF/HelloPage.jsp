e3ae0ae6f5142bc8711b075d8c301c01613179c8
Hiero <kazsik.adam@gmail.com>
2016-01-08 13:55:21 +0100

    add annotations

--- WebContent/WEB-INF/HelloPage.jsp
+++ WebContent/WEB-INF/HelloPage.jsp
@@ -3,7 +3,7 @@
 	
 		<h1>First Spring MVC Application Demo</h1>
 		
-		<h2>${welcomeMessage}</h2>
+		<h2>${msg}</h2>
 
 	</body>
 </html>
--- src/com/hiero/hellocontroller/HelloController.java
+++ src/com/hiero/hellocontroller/HelloController.java
@@ -1,20 +1,19 @@
 package com.hiero.hellocontroller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
-public class HelloController extends AbstractController{
-
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request, 
-												 HttpServletResponse response) throws Exception {
-		ModelAndView modelandview = new ModelAndView("HelloPage");
-		modelandview.addObject("welcomeMessage", "Hi user, welcome to the fist Spring MVC Application!");
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld(){
 		
-		return modelandview;
+		ModelAndView model = new ModelAndView("HelloPage");
+		model.addObject("msg", "Hello World!");
+		
+		return model;
+		
 	}
-
 }
