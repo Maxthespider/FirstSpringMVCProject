a8a49eb22781bb207e63a39f8da3dfb864667545
Hiero <kazsik.adam@gmail.com>
2016-01-08 17:39:27 +0100

    try @ModelAttribute annotation

--- WebContent/WEB-INF/AdmissionSuccess.jsp
+++ WebContent/WEB-INF/AdmissionSuccess.jsp
@@ -1,7 +1,20 @@
 <html>
 	<body>
-		<h1>Congratulations! The Engineering college has processed your Application form succesfully!</h1>
+		<h1>${headerMessage}</h1>
+		<h3>Congratulations! The Engineering college has processed your Application form succesfully!</h3>
 	
-		<h2>${msg2}</h2>
+		<h2>Details submitted by you: </h2>
+		
+		<table>
+			<tr>
+				<td>Student Name :</td>
+				<td>${student1.studentName}</td>
+			</tr>
+			<tr>
+				<td>Student Hobby :</td>
+				<td>${student1.studentHobby}</td>
+			</tr>
+		</table>
+	
 	</body>
 </html>
--- src/com/hiero/studentadmissioncontroller/StudentAdmissionController.java
+++ src/com/hiero/studentadmissioncontroller/StudentAdmissionController.java
@@ -1,11 +1,9 @@
 package com.hiero.studentadmissioncontroller;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -21,14 +19,15 @@ public class StudentAdmissionController {
 	
 	@RequestMapping(value="/submitAdmissionForm.html", method = RequestMethod.POST)
 	//public ModelAndView submitAdmissionForm(@RequestParam(value="studentName", defaultValue="Mr. ABC") String name, @RequestParam("studentHobby") String hobby){
-	public ModelAndView submitAdmissionForm(@RequestParam Map<String,String> reqPar){
-		
-		String name = reqPar.get("studentName");
-		String hobby = reqPar.get("studentHobby");
+	public ModelAndView submitAdmissionForm(@ModelAttribute("student1") Student student1){	
+		//Student student1 = new Student();
+		//student1.setStudentName(name);
+		///student1.setStudentHobby(hobby);
 		
 		ModelAndView model = new ModelAndView("AdmissionSuccess");
-		model.addObject("msg2", "Details submitted by you: Name: " + name + ", Hobby: " + hobby);
-		 
+		model.addObject("headerMessage", "Hiero College of Engineering, Hungary");
+		model.addObject("student1", student1);
+		
 		return model;
 	}
 	
